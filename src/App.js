dc1f5e34d3832d8957d39164b94028cd60a11c92
muzaahmadk2 <muzaahmadk2@gmail.com>
2023-04-28 16:05:24 +0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { lazy, useState } from "react";
 import Cart from "./Components/Cart/Cart";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
 import CartProvider from "./Store/CartProvider";
+
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
